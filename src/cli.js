9c46067d2f5b1f40bdd782e394294466a06847b3
Christian Murphy <Christian.Murphy.42@gmail.com>
2018-11-14 11:07:48 -0700

    feat: support passing authorization token to rest service

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,16 @@
 const { ApolloServer } = require("apollo-server");
-const { typeDefs, resolvers } = require("./index.js");
+const { typeDefs, resolvers, LayoutAPI } = require("./index.js");
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  dataSources: () => ({
+    LayoutAPI: new LayoutAPI()
+  }),
+  context: ({ req }) => ({
+    authorization: req.headers.authorization
+  })
+});
 
 server.listen().then(({ url }) => {
   // eslint-disable-next-line no-console
--- src/index.js
+++ src/index.js
@@ -1,15 +1,49 @@
 const { gql } = require("apollo-server");
+const { RESTDataSource } = require("apollo-datasource-rest");
+
+class LayoutAPI extends RESTDataSource {
+  constructor() {
+    super();
+    this.baseURL = "http://localhost:8080/uPortal/api";
+  }
+
+  willSendRequest(request) {
+    request.headers.set("Authorization", this.context.authorization);
+  }
+
+  async getLayout() {
+    return this.get("/v4-3/dlm/layout.json");
+  }
+}
+
+exports.LayoutAPI = LayoutAPI;
 
 // Construct a schema, using GraphQL schema language
 exports.typeDefs = gql`
   type Query {
-    hello: String
+    layout: Layout
+  }
+
+  type Layout {
+    user: String
+    authenicated: Boolean
+    regions: [Region]
+  }
+
+  type Region {
+    name: String
+    content: [Content]
+  }
+
+  type Content {
+    url: String
   }
 `;
 
 // Provide resolver functions for your schema fields
 exports.resolvers = {
   Query: {
-    hello: () => "Hello world!"
+    layout: (_sources, _attrs, { dataSources }) =>
+      dataSources.LayoutAPI.getLayout()
   }
 };
