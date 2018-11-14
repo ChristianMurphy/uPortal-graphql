0e3cf066c367469bdfd97fc85d5aa51f677f5eb7
Christian Murphy <Christian.Murphy.42@gmail.com>
2018-11-13 17:06:16 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!'
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  // eslint-disable-next-line no-console
+  console.log(`🚀 Server ready at ${url}`)
+});
