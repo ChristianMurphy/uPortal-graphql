02d9891909e8fb2615ea404210ddbaca46e63575
Christian Murphy <Christian.Murphy.42@gmail.com>
2018-11-14 11:12:57 -0700

    fix: update lambda version of code

--- src/lambda.js
+++ src/lambda.js
@@ -1,6 +1,15 @@
 const { ApolloServer } = require("apollo-server");
-const { typeDefs, resolvers } = require("./index.js");
+const { typeDefs, resolvers, LayoutAPI } = require("./index.js");
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  dataSources: () => ({
+    LayoutAPI: new LayoutAPI()
+  }),
+  context: ({ req }) => ({
+    authorization: req.headers.authorization
+  })
+});
 
 exports.handler = server.createHandler();
